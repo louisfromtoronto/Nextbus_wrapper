c83c1e2a298a8c0fcafb2b160eabc25c4950379c
louisfromtoronto <31932581+louisfromtoronto@users.noreply.github.com>
2018-01-07 00:45:37 -0500

    getRouteListForAgency() and getAgencyList() are now functional

--- index.js
+++ index.js
@@ -1,45 +1,74 @@
-console.log("index.js is running");
-
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-
-const AGENCY_LIST_URL = "http://webservices.nextbus.com/service/publicXMLFeed?command=agencyList";
-
+var DOMParser = require('xmldom').DOMParser;
+const fs = require('fs');
 const URL = "http://webservices.nextbus.com/service/publicXMLFeed?";
 const COMMAND_AGENCY_LIST = "command=agencyList";
 const QUERY_URL = URL + COMMAND_AGENCY_LIST; 
 
+var xmlDom;
+// onreadystatechange specified a function to be executed
+// every time the status of the XMLHTTPRequest object changes.
 
-function makeXHTTPRequest(callback) {
+// Before an XML document can be accessed, it must be loaded into
+// an XML DOM object.
+
+/// Requests the agency list, and returns it at as an array of JSON-formatted
+/// strings, specifying the tag, title and regionTitle of each agency.
+function getAgencyList() {
+    console.log('getAgencyList()');
+    let url = "http://webservices.nextbus.com/service/publicXMLFeed?command=agencyList"
+
     var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-        callback(this);
-        }
-    };
-    xhttp.open("GET", QUERY_URL, true);
+
+    xhttp.open("GET", url, false);
     xhttp.send();
+    xmlDom = loadStringToXMLDOM(xhttp.responseText);
+    console.log(xmlDom.getElementsByTagName("agency")[0].getAttribute("title"))
+
+    var json;
+
+    var resultsArray = [];
+    for (var i = 0; i < xmlDom.getElementsByTagName("agency").length; i++) {
+        let agency = xmlDom.getElementsByTagName("agency")[i];
+        json = { title: agency.getAttribute("title"),
+                       tag: agency.getAttribute("tag"),
+               regionTitle: agency.getAttribute("regionTitle")};
+        resultsArray.push(json);
+    }
+
+    return resultsArray;
 }
 
-function XMLHander(xml) {
-    console.log(xml);
+// Requests the route list for a specified agency, and returns it as a set of key-value pairs
+// tag: title
+function getRouteListForAgency(agencyTag) {
+    let url = "http://webservices.nextbus.com/service/publicXMLFeed?command=routeList&a="
+    url += agencyTag;
+    let xhttp = makeXMLHTTPRequest("GET", url);
+    let xmlDom = loadStringToXMLDOM(xhttp.responseText);
+    let routes = xmlDom.getElementsByTagName("route");
+    // console.log(routes);
+    var resultsDictionary = {};
+    console.log(routes[0].getAttribute("tag"));
+    for (var i = 0; i < routes.length; i++) {
+        resultsDictionary[routes[i].getAttribute("tag")] = routes[i].getAttribute("title");
+    }
+    return resultsDictionary;
 }
 
-makeXHTTPRequest(XMLHander);
+function loadStringToXMLDOM(string) {
+    var parser = new DOMParser();
+    var xmlDoc = parser.parseFromString(string, "application/xml");
+    return xmlDoc;
+}
 
+// Makes a new XMLHTTPRequest with specified method and URL, and returns the 
+// XMLHTTRequest object once communication with the server is complete.
+function makeXMLHTTPRequest(method, url){
+    var xhttp = new XMLHttpRequest();
+    xhttp.open(method, url, false);
+    xhttp.send();
+    return xhttp;
+}
 
-// var xhttp = new XMLHttpRequest();
-// xhttp.onreadystatechange = function() {
-//     if (this.readyState == 4 && this.status == 200) {
-//     myFunction(this);
-//     }
-// };
-// xhttp.open("GET", QUERY_URL, true);
-// xhttp.send();
-
-// function myFunction(xml) {
-//     // var xmlDoc = xml.responseXML;
-//     // document.getElementById("demo").innerHTML =
-//     // xmlDoc.getElementsByTagName("title")[0].childNodes[0].nodeValue;
-
-//     console.log(xml);
-// }
+getRouteListForAgency("ttc");
